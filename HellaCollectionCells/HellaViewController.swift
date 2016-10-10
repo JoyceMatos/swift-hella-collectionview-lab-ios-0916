954c0b039de0bc70405bfad1531090e784211043
JoyceMatos <j.matosjoyce@gmail.com>
2016-10-10 13:39:05 -0400

    Completed

--- HellaCollectionCells/HellaViewController.swift
+++ HellaCollectionCells/HellaViewController.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 
-class HellaViewController: UIViewController {
-
+class HellaViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var hellaCollectionView: UICollectionView!
+    
+    let fiboArray = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+    
     override func viewDidLoad() {
-    // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+        hellaCollectionView.delegate = self
+        hellaCollectionView.dataSource = self
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
+        
+        if fiboArray.contains(indexPath.row) {
+            cell.backgroundColor = UIColor.purple
+        } else {
+            cell.backgroundColor = UIColor.yellow
+        }
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if
+            let destination = segue.destination as? HellaDetailViewController,
+            let indexPath = hellaCollectionView.indexPath(for: sender as! UICollectionViewCell) {
+            destination.indexNumber = "\(indexPath.item)"
+            destination.color = (hellaCollectionView.cellForItem(at: indexPath)?.backgroundColor)!
+            
+        }
+        
+    }
+    
 }
 
---
+++ HellaDetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  HellaDetailViewController.swift
+//  HellaCollectionCells
+//
+//  Created by Joyce Matos on 10/10/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class HellaDetailViewController: UIViewController {
+
+    @IBOutlet weak var indexNumberLabel: UILabel!
+    @IBOutlet weak var backgroundColor: UIView!
+    
+    var indexNumber = String()
+    var color = UIColor()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        indexNumberLabel.text = indexNumber
+        backgroundColor.backgroundColor = color
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
